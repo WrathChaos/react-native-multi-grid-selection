501ab4b340ad4ccf1491999eeb46592e23bf2bf3
FreakyCoder <kurayogun@gmail.com>
2021-03-05 16:03:04 +0300

    feat: Single select logic is implemented

--- example/lib/MultiGridSingleSelect.tsx
+++ example/lib/MultiGridSingleSelect.tsx
@@ -1,14 +1,19 @@
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import React, { Component } from "react";
+import { Text, View } from "react-native";
 /**
  * ? Local Imports
  */
-import styles from './MultiGridSingleSelect.style';
-import Card from './components/Card/Card';
+import styles from "./MultiGridSingleSelect.style";
+import Card from "./components/Card/Card";
 
 interface IMultiGridSingleSelectProps {}
 
 interface IState {}
+
+const data = [
+  { id: 0, name: "Every week" },
+  { id: 1, name: "Every other week" },
+];
 
 export default class MultiGridSingleSelect extends Component<
   IMultiGridSingleSelectProps,
@@ -17,7 +22,12 @@ export default class MultiGridSingleSelect extends Component<
   render() {
     return (
       <View>
-        <Card />
+        <Card
+          data={data}
+          onPress={(clicked: any) => {
+            console.log("Clicked: ", clicked);
+          }}
+        />
       </View>
     );
   }
--- example/lib/components/Card/Card.tsx
+++ example/lib/components/Card/Card.tsx
@@ -1,29 +1,58 @@
 import React from "react";
-import { View, Text, StyleProp, ViewStyle } from "react-native";
+import { View, Dimensions, FlatList, StyleProp, ViewStyle } from "react-native";
 /**
  * ? Local Imports
  */
 import styles from "./Card.style";
 import SelectButton from "../SelectButton/SelectButton";
 
+const { width: ScreenWidth } = Dimensions.get("screen");
 type CustomStyleProp = StyleProp<ViewStyle> | Array<StyleProp<ViewStyle>>;
+
+export interface IButtonDataType {
+  id: number;
+  name: string;
+  data?: any;
+}
 
 interface ICardProps {
   style?: CustomStyleProp;
+  data: Array<IButtonDataType>;
+  onSelect: (buttonData: IButtonDataType) => void;
 }
 
-const Card: React.FC<ICardProps> = ({ style }) => {
-  const [isActive, setIsActive] = React.useState(false);
+const Card: React.FC<ICardProps> = ({ style, data, onSelect }) => {
+  const [selectedItem, setSelectedItem] = React.useState<
+    IButtonDataType | undefined
+  >(undefined);
+
+  const handleOnSelectItem = (item: IButtonDataType) => {
+    setSelectedItem(item);
+    onSelect && onSelect(item);
+  };
+
+  const renderButton = (data: any) => {
+    const { id, name } = data.item;
+    const isActive = id === selectedItem?.id;
+    return (
+      <SelectButton
+        text={name}
+        isActive={isActive}
+        onPress={() => handleOnSelectItem(data.item)}
+      />
+    );
+  };
+
   return (
     <View style={[styles.container, style]}>
-      <SelectButton
-        text="Every week"
-        isActive={isActive}
-        onPress={() => setIsActive(!isActive)}
+      <FlatList
+        data={data}
+        renderItem={renderButton}
+        contentContainerStyle={{
+          paddingLeft: 8,
+          paddingRight: 8,
+        }}
       />
-      <SelectButton text="Every other week" onPress={() => {}} />
-      <SelectButton text="Twice per month" onPress={() => {}} />
-      <SelectButton text="Every month" onPress={() => {}} />
     </View>
   );
 };
