476687e893649a138a7d22c5d8ec4bd72e008221
FreakyCoder <kurayogun@gmail.com>
2021-03-05 16:15:03 +0300

    feat: Main multi grid flat list is integrated with dummy way

--- example/lib/MultiGridSingleSelect.tsx
+++ example/lib/MultiGridSingleSelect.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { FlatList, View } from "react-native";
 /**
  * ? Local Imports
  */
@@ -10,24 +10,44 @@ interface IMultiGridSingleSelectProps {}
 
 interface IState {}
 
-const data = [
-  { id: 0, name: "Every week" },
-  { id: 1, name: "Every other week" },
+const newData = [
+  [
+    { id: 0, name: "Every week" },
+    { id: 1, name: "Every other week" },
+  ],
+  [
+    { id: 0, name: "Every week2" },
+    { id: 1, name: "Every other week2" },
+  ],
+  [
+    { id: 0, name: "Every week3" },
+    { id: 1, name: "Every other week3" },
+  ],
+  [
+    { id: 0, name: "Every week4" },
+    { id: 1, name: "Every other week5" },
+  ],
 ];
 
 export default class MultiGridSingleSelect extends Component<
   IMultiGridSingleSelectProps,
   IState
 > {
+  renderCard = (data: any) => {
+    return (
+      <Card
+        data={data.item}
+        onSelect={(selectedData: ISelectionDataType) => {
+          console.log("onSelect: ", selectedData);
+        }}
+      />
+    );
+  };
+
   render() {
     return (
       <View>
-        <Card
-          data={data}
-          onSelect={(selectedData: ISelectionDataType) => {
-            console.log("onSelect: ", selectedData);
-          }}
-        />
+        <FlatList horizontal data={newData} renderItem={this.renderCard} />
       </View>
     );
   }
